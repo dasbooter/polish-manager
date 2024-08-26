5d6b70a1985dc2bc988096e03f1741de5f62fe0f
Rasputin <dasturboboot@gmail.com>
2024-08-25 21:27:00 -0500

    combined add, remove and edit database together into a simpler single menu

--- nail_polish_gui.py
+++ nail_polish_gui.py
@@ -8,7 +8,7 @@ class NailPolishApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Polish Manager by Распутин")
-        self.root.geometry("325x350")
+        self.root.geometry("325x200")
         self.current_info_window = None
 
         self.db = PolishDatabase()
@@ -25,10 +25,7 @@ class NailPolishApp:
     def create_buttons(self):
         tk.Button(self.root, text="Review Inventory", command=lambda: self.view_items("inventory")).pack(pady=10)
         tk.Button(self.root, text="Wishlist", command=lambda: self.view_items("wishlist")).pack(pady=10)
-        tk.Button(self.root, text="Search Polish Database", command=self.search_polishes).pack(pady=10)
-        tk.Button(self.root, text="Add New Polish to Database", command=self.add_polish_to_database).pack(pady=10)
-        tk.Button(self.root, text="Edit Database", command=self.edit_polish_in_database).pack(pady=10)
-        tk.Button(self.root, text="Remove Polish from Database", command=self.remove_polish_from_database).pack(pady=10)
+        tk.Button(self.root, text="Database", command=self.edit_polish_in_database).pack(pady=10)
         tk.Button(self.root, text="Exit", command=self.root.quit).pack(pady=10)
 
     def show_message(self, title, message):
@@ -66,53 +63,6 @@ class NailPolishApp:
         else:
             self.show_message(f"Your {item_type.capitalize()}", f"Your {item_type} is empty.")
 
-    def search_polishes(self):
-        search_window = tk.Toplevel(self.root)
-        search_window.title("Search Known Polishes")
-
-        polish_form = PolishForm(search_window, self.db)
-
-        def search_results():
-            data = polish_form.get_form_data()
-
-            results = self.db.search_polish(**data)
-
-            if results:
-                result_window = tk.Toplevel(search_window)
-                result_window.title("Search Results")
-
-                for idx, polish in enumerate(results):
-                    frame = tk.Frame(result_window)
-                    frame.pack(fill='x', pady=5)
-
-                    tk.Label(frame, text=str(polish)).pack(side="left", padx=10)
-                    tk.Button(frame, image=self.heart_icon, command=lambda p=polish: self.add_to_wishlist(p)).pack(side="right")
-                    tk.Button(frame, text="Add to Inventory", command=lambda p=polish: self.add_to_inventory_and_close(p, result_window)).pack(side="right")
-            else:
-                self.show_message("Search Results", "No polishes found matching the criteria.")
-
-        tk.Button(search_window, text="Search", command=search_results).grid(row=12, column=0, columnspan=2, pady=10)
-
-    def add_polish_to_database(self):
-        add_window = tk.Toplevel(self.root)
-        add_window.title("Add New Polish to Database")
-
-        polish_form = PolishForm(add_window, self.db)
-
-        def save_polish():
-            data = polish_form.get_form_data()
-
-            if not data["name"]:
-                self.show_message("Error", "Polish name is required.")
-                return
-
-            new_polish = Polish(**data)
-            self.db.manage_polish(new_polish, "add")
-            self.show_message("Success", "New polish added to the database.")
-            add_window.destroy()
-
-        tk.Button(add_window, text="Add Polish", command=save_polish).grid(row=12, column=0, columnspan=2, pady=10)
-
     def edit_polish_in_database(self):
         edit_window = tk.Toplevel(self.root)
         edit_window.title("Edit Polish in Database")
@@ -126,40 +76,50 @@ class NailPolishApp:
 
             if results:
                 result_window = tk.Toplevel(edit_window)
-                result_window.title("Select Polish to Edit")
+                result_window.title("Search Results")
 
                 for idx, polish in enumerate(results):
                     frame = tk.Frame(result_window)
                     frame.pack(fill='x', pady=5)
 
                     tk.Label(frame, text=str(polish)).pack(side="left", padx=10)
-                    tk.Button(frame, text="Edit", command=lambda p=polish: self.edit_polish_form(p, result_window)).pack(side="right")
+                    tk.Button(frame, image=self.plus_icon, command=lambda p=polish: self.add_to_inventory_and_close(p, result_window)).pack(side="right")
+                    tk.Button(frame, image=self.minus_icon, command=lambda p=polish: self.remove_selected_polish(p, result_window)).pack(side="right")
+                    tk.Button(frame, image=self.heart_icon, command=lambda p=polish: self.add_to_wishlist(p)).pack(side="right")
             else:
                 self.show_message("Search Results", "No polishes found matching the criteria.")
 
-        tk.Button(edit_window, text="Search", command=search_and_edit).grid(row=12, column=0, columnspan=2, pady=10)
-
-    def remove_polish_from_database(self):
-        remove_window = tk.Toplevel(self.root)
-        remove_window.title("Remove Polish from Database")
-
-        polish_form = PolishForm(remove_window, self.db)
-
-        def search_and_remove():
+        def add_polish():
             data = polish_form.get_form_data()
 
-            results = self.db.search_polish(**data)
+            if not data["name"]:
+                self.show_message("Error", "Polish name is required.")
+                return
 
-            if results:
-                result_window = tk.Toplevel(remove_window)
-                result_window.title("Select Polish to Remove")
+            new_polish = Polish(**data)
+            self.db.manage_polish(new_polish, "add")
+            self.show_message("Success", "New polish added to the database.")
 
-                for idx, polish in enumerate(results):
-                    tk.Button(result_window, text=str(polish), command=lambda p=polish: self.remove_selected_polish(p, result_window)).pack(pady=5)
+        def show_all():
+            all_polishes = self.db.display_all()
+            if all_polishes:
+                result_window = tk.Toplevel(edit_window)
+                result_window.title("All Polishes in Database")
+
+                for idx, polish in enumerate(all_polishes):
+                    frame = tk.Frame(result_window)
+                    frame.pack(fill='x', pady=5)
+
+                    tk.Label(frame, text=str(polish)).pack(side="left", padx=10)
+                    tk.Button(frame, image=self.plus_icon, command=lambda p=polish: self.add_to_inventory_and_close(p, result_window)).pack(side="right")
+                    tk.Button(frame, image=self.minus_icon, command=lambda p=polish: self.remove_selected_polish(p, result_window)).pack(side="right")
+                    tk.Button(frame, image=self.heart_icon, command=lambda p=polish: self.add_to_wishlist(p)).pack(side="right")
             else:
-                self.show_message("Search Results", "No polishes found matching the criteria.")
+                self.show_message("Database", "The database is empty.")
 
-        tk.Button(remove_window, text="Search", command=search_and_remove).grid(row=12, column=0, columnspan=2, pady=10)
+        tk.Button(edit_window, text="Search", command=search_and_edit).grid(row=12, column=0, pady=5)
+        tk.Button(edit_window, text="Add Polish to Database", command=add_polish).grid(row=12, column=1, pady=5)
+        tk.Button(edit_window, text="Show All", command=show_all).grid(row=12, column=2, columnspan=2, pady=5)
 
     def remove_selected_polish(self, polish, result_window):
         self.db.manage_polish(polish, "remove")
